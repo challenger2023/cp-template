434c1cab6adb4c9b6e60e14ed496b500908afed3
Ian Pau <psycode1234@gmail.com>
2023-08-19 17:16:32 -0700

    fixed

--- Data Structures/sparse table.hpp
+++ Data Structures/sparse table.hpp
@@ -27,3 +27,34 @@ class SPARSE{
 		int sz, depth;
 		vector<vector<int>> st;
 };
+
+struct st{
+    int sz, d;
+    vector<vector<int>> table;
+    void init(int &pa) {
+        sz = pa;
+        d = (int)ceil(log2(sz));
+        table.resize(d+1);
+        for (int i = 0; i <= sz; i++) table[i].resize(sz);
+    }
+    void update(int &it, int &val) {
+        table[0][it] = val;
+    }
+    /**************************************************/
+    int fun(int &a, int &b) {
+        return max(a, b);
+    }
+    /**************************************************/
+    void build() {
+        for (int i = 1; i <= d; i++) {
+            for (int j = 0; j + (1 << i) <= sz; j++) {
+                table[i][j] = fun(table[i-1][j], table[i-1][j + (1 << i)]);
+            }
+        }
+    }
+    int query(int &l, int &r) {
+        if (l == r) return table[0][l];
+        int k = (int)floor(log2(r-l));
+        return fun(table[k][l], table[k][r - (1 << k) + 1]);
+    }
+};
